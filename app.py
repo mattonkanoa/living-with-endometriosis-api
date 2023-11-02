536d96e22b60737662454d877e6109110d1dd1e4
Kanoa Matton <kanoa.matton@atmosphere.tv>
2023-11-01 21:00:01 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,13 @@
+import json
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+employees = [ { 'id': 1, 'name': 'Kanoa' }, { 'id': 2, 'name': 'Kai' }, { 'id': 3, 'name': 'Moana' }]
+
+@app.route('/employees', methods=['GET'])
+def get_employees():
+    return jsonify(employees)
+
+if __name__ == '__main__':
+    app.run(port=5000)
