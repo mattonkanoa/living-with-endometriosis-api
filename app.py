5b6e36c60f2e792ad8bb2eea6527b54890ca61c8
Kanoa Matton <kanoa.matton@atmosphere.tv>
2023-11-01 21:34:20 -0500

    added gunicorn

--- app.py
+++ app.py
@@ -3,11 +3,8 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-employees = [ { 'id': 1, 'name': 'Kanoa' }, { 'id': 2, 'name': 'Kai' }, { 'id': 3, 'name': 'Moana' }]
+employees = [ { 'id': 1, 'name': 'Kanoa Matton' }, { 'id': 2, 'name': 'Kai' }, { 'id': 3, 'name': 'Moana' }]
 
 @app.route('/employees', methods=['GET'])
 def get_employees():
     return jsonify(employees)
-
-if __name__ == '__main__':
-    app.run()
