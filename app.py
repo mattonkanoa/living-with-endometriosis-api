9c3abdfc94fe0ee628c69e55cf822611308d8973
kaimatton <kaimatton10@gmail.com>
2024-01-27 15:32:37 -0600

    Changed Kanoa to Kalani

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-employees = [ { 'id': 1, 'name': 'Kanoa Matton' }, { 'id': 2, 'name': 'Kai' }, { 'id': 3, 'name': 'Moana' }]
+employees = [ { 'id': 1, 'name': 'Kalani Matton' }, { 'id': 2, 'name': 'Kai' }, { 'id': 3, 'name': 'Moana' }]
 
 @app.route('/employees', methods=['GET'])
 def get_employees():
