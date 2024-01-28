305509990695e6b31d463ab64a5a5e15022add9d
Kanoa Matton <kanoa.matton@atmosphere.tv>
2024-01-28 16:07:58 -0600

    - added spoonacular api - setup empty routes - added gitignore file

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 import json
+import config
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
+spoonacularApiKey = config.spoonacularApiKey
 
-employees = [ { 'id': 1, 'name': 'Kalani Matton' }, { 'id': 2, 'name': 'Kai' }, { 'id': 3, 'name': 'Moana' }]
+# api route for fetching recipes https://spoonacular.com/food-api/docs#Search-Recipes-Complex
+@app.route('/search-recipes', methods=['GET'])
+def getRecipes():
+    return True
 
-@app.route('/employees', methods=['GET'])
-def get_employees():
-    diet = request.args.get('diet')
-    return diet
+# api route for similar recipes https://spoonacular.com/food-api/docs#Get-Similar-Recipes
+@app.route('/search-similar-recipes/<recipeId>', methods=['GET'])
+def getSimilarRecipes():
+    return True
+
+# api route for random recipes https://spoonacular.com/food-api/docs#Get-Random-Recipes
