15925aff759cac11c68f3624eaf3e80cda12d2ca
Kanoa Matton <kanoa.matton@atmosphere.tv>
2024-02-10 16:24:41 -0600

    get recipes api

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import config
-
-from flask import Flask
+import requests
+from flask import Flask, request
 
 app = Flask(__name__)
 spoonacularApiKey = config.spoonacularApiKey
@@ -8,7 +8,17 @@ spoonacularApiKey = config.spoonacularApiKey
 # api route for fetching recipes https://spoonacular.com/food-api/docs#Search-Recipes-Complex
 @app.route('/search-recipes', methods=['GET'])
 def getRecipes():
-    return True
+    # The food type e.g. rice, pasta, steak
+    foodType = request.args.get('query') 
+    cuisine = request.args.get('cuisine') 
+    diet = request.args.get('diet')
+    maxCalories = request.args.get('maxCalories')
+
+    # Build url - TEST
+    urlString = f"https://api.spoonacular.com/recipes/complexSearch?query={foodType}&number=10&apiKey={spoonacularApiKey}"
+    results = requests.get(urlString)
+
+    return results
 
 # api route for similar recipes https://spoonacular.com/food-api/docs#Get-Similar-Recipes
 @app.route('/search-similar-recipes', methods=['GET'])
